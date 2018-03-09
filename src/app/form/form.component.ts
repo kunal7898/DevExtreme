4d6e9bda177e40f5be0d955761cab48bedbe6bbb
Kunal Singh <kunal.singh@excellonsoft.com>
2018-03-09 16:48:54 +0530

    URL Validation

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,192 @@
+import { Component, OnInit ,ViewChild} from '@angular/core';
+
+import { FormViewResolver } from './formViewResolver';
+import { Tab, TabServices } from '../tab.service';
+import { DataGridComponent } from '../data-grid/data-grid.component';
+import { DataGridService } from '../data-grid.service';
+import { GridColumns } from '../data-grid/data-grid.columns';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  @ViewChild(DataGridComponent) private DataGridComponent :DataGridComponent;
+  FirstgroupCount:number=0;
+  SecondGroupCount:number=0;
+  DataSource :any=JSON.parse(localStorage.getItem('3'))
+  formData:{};
+  items :any[];
+  public tabs: Tab[];
+  constructor(TabService:TabServices,private Datagridservice:DataGridService) {
+    this.tabs = TabService.getTabs();
+
+   }
+
+
+  ngOnInit() {
+    this.formData=this.SetFormData();
+    this.items =this.LoadInnerItems(this.LoadHeaderItems());
+    this.DataGridComponent.SetData( this.Datagridservice.getEmployees());
+    this.DataGridComponent.SetColumn(GridColumns.getColumns());
+  }
+
+public SetFormData(){
+  var Obj = {"ID":"","FirstName":"","LastName":"","Position":"","BirthDate":"","HireDate":"","Address":"","State":"","ZipCode":"","Notes":""};
+  return Obj;
+  
+}
+  
+public LoadHeaderItems():Array<object>{
+  
+  let viewresolver = new FormViewResolver();
+  let Items = Array<object>();
+  viewresolver.LoadMetaData().forEach(element => {
+    if(element["cssClass"]=="first-group" && this.FirstgroupCount==0){
+      Items.push({
+        cssClass:element["cssClass"],
+        colCount:element["colCount"],
+        itemType:"group",
+        items:[]
+      })
+      this.FirstgroupCount++;
+    }
+    if(element["cssClass"]=="second-group" && this.SecondGroupCount==0){
+      Items.push({
+        cssClass:element["cssClass"],
+        colCount:element["colCount"],
+        itemType:"group",
+        items:[]
+      })
+      this.SecondGroupCount++;
+    }
+});
+
+return Items;
+}
+
+
+public SetTemplate(template:any){
+
+  return 
+}
+
+public LoadInnerItems(Inneritems:any):Array<object>{
+  let viewresolver = new FormViewResolver();
+
+  viewresolver.LoadMetaData().forEach(element => {
+    if(this.FirstgroupCount==1 && element["cssClass"]=="first-group"){
+      Inneritems[0].items.push({
+        dataField:element["code"],
+        editorType:this.getEditorType(element["AttributeType"]),
+        editorOptions:this.getEditorOptions(this.getEditorType(element["AttributeType"]),element["PicklistId"],element["code"]),
+        validationRules: this.getMandatoryFieldsValidation(element["code"],element["IsMandatory"],element["length"],element["IsCustomValidation"],element["validationCallback"]) ,
+      })
+    }
+    if(this.SecondGroupCount==1 && element["cssClass"]=="second-group"){
+      Inneritems[1].items.push({
+        dataField:element["code"],
+        editorType:this.getEditorType(element["AttributeType"]),
+        editorOptions:this.getEditorOptions(this.getEditorType(element["AttributeType"]),element["PicklistId"],element["code"]),
+        validationRules: this.getMandatoryFieldsValidation(element["code"],element["IsMandatory"],element["length"],element["IsCustomValidation"],element["validationCallback"]) ,
+      })
+    }
+});
+
+return Inneritems;
+}
+
+
+
+
+
+public getMandatoryFieldsValidation(Code,IsMandatory,length,IsCustomValidation,validationCallback):any{
+let  validationRule =  Array<object>();
+  if(IsMandatory==true)
+  {
+    validationRule.push({
+      type: "required",
+      message: Code+" is required"
+    })
+  }
+  if(length!=null){
+    validationRule.push({
+      type: 'stringLength', min: 4, max: 6, message: 'Zip  length should be in a range from 4 to 6 symbols.'
+    })
+  }
+
+  if(IsCustomValidation){
+    validationRule.push({
+      type: 'pattern', pattern: '^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$', message: 'Invalid URl'
+    })
+  }
+
+  return validationRule
+
+ }
+ 
+
+ public validationCallback(options){
+ 
+  if (options.value.length < 4) {
+    options.rule.message = "The password is too short. The password length must be more than 4.";
+    return false;
+} else { return true; }
+ }
+ public getEditorType(Attributetype):any{
+   if(Attributetype=="lookup")
+    return "dxSelectBox";
+    if(Attributetype=="Date")
+    return "dxDateBox";
+    if(Attributetype=="textarea")
+    return "dxTextArea";
+    else
+    return null;
+ 
+ }
+
+ public getEditorOptions(Type,PicklistId,Code):any{
+  if(Type=="dxSelectBox" && Code!='City')
+    return  {
+      items:JSON.parse(localStorage.getItem(PicklistId)),
+      displayExpr: "Name",
+      valueExpr: "ID",
+      searchEnabled: true,
+      onSelectionChanged:function(e){
+        window.alert("event fired");
+        this.popupVisible=false;
+       } ,
+       onValueChanged:function(e){
+         var get  = this.DataSource;
+        window.alert("event fired");
+        this.popupVisible=false;
+       }
+    };  
+    if(Type=="dxSelectBox" && Code=='City')
+    return  {
+      items:this.DataSource,
+      displayExpr: "Name",
+      valueExpr: "ID",
+      searchEnabled: true,
+      onSelectionChanged:function(e){
+        window.alert("event fired");
+        this.popupVisible=false;
+       } ,
+       onValueChanged:function(e){
+        window.alert("event fired");
+        this.popupVisible=false;
+       } ,onOpened:function(e){
+        function StateID(element, index, array) { 
+          return (element.StateID==1); 
+       } 
+       e.component._dataSource._storeLoadOptions.filter=e.component._options.items.filter(StateID)
+       }
+    };
+  else
+  return null;
+}
+
+ 
+
+}
---
+++ src/app/form/formViewResolver.ts
@@ -0,0 +1,24 @@
+export class FormViewResolver{
+
+
+    public LoadMetaData():Array<object>{
+        let Values =  new Array<object>();
+        Values.push(
+        {code:'ID',Name:'ID',AttributeType:'number',PicklistId:null,IsMandatory:true,colCount: 4, cssClass: "first-group",length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'FirstName',Name:'First Name',AttributeType:'string',PicklistId:null,IsMandatory:true, cssClass: "first-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'LastName',Name:'Last Name',AttributeType:'string',PicklistId:null,IsMandatory:true, cssClass: "first-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'Position',Name:'Position',AttributeType:'lookup',PicklistId:2,IsMandatory:true, cssClass: "first-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'BirthDate',Name:'Date Of birth',AttributeType:'Date',PicklistId:null,IsMandatory:true, cssClass: "first-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'HireDate',Name:'Hire Date',AttributeType:'Date',PicklistId:null,IsMandatory:true, cssClass: "first-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'URl',Name:'URL',AttributeType:'string',PicklistId:null,IsMandatory:true, cssClass: "first-group",colCount: null,length:null,IsCustomValidation:true,validationCallback:'validationCallback'},
+        {code:'Address',Name:'Address',AttributeType:'string',PicklistId:null,IsMandatory:true, cssClass: "second-group",colCount: 2,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'State',Name:'State',AttributeType:'lookup',PicklistId:1,IsMandatory:true, cssClass: "second-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'City',Name:'City',AttributeType:'lookup',PicklistId:3,IsMandatory:true, cssClass: "second-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
+        {code:'ZipCode',Name:'Zip Code',AttributeType:'string',PicklistId:null,IsMandatory:true, cssClass: "second-group",colCount: null,length:6,IsCustomValidation:false,validationCallback:null},
+        {code:'Notes',Name:'Notes',AttributeType:'textarea',PicklistId:null,IsMandatory:true, cssClass: "second-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null}
+      )
+        return Values;
+      }
+
+     
+}
