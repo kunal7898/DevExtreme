c1a081b50ae32f64705fea130d97cc5c589ec034
Kunal Singh <kunal.singh@excellonsoft.com>
2018-04-02 12:01:23 +0530

    changes

--- src/app/draggable/draggable.component.ts
+++ src/app/draggable/draggable.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TreeViewService } from '../tree-view/Tree-view.service';
-import {DevExtremeModule} from 'devextreme-angular';
+import {DevExtremeModule,DxSlideOutViewComponent} from 'devextreme-angular';
 
 
 
@@ -8,7 +8,8 @@ import {DevExtremeModule} from 'devextreme-angular';
 @Component({
   selector: 'app-draggable',
   templateUrl: './draggable.component.html',
-  styleUrls: ['./draggable.component.css']
+  styleUrls: ['./draggable.component.css'],
+  encapsulation:ViewEncapsulation.None,
 })
 export class DraggableComponent implements OnInit {
   dataSource: any[];
@@ -16,7 +17,8 @@ export class DraggableComponent implements OnInit {
   toolbarItems: any[];
   width : number = 500;
   height :number = 500;
-
+  formData: any;
+  items: any[];
   Items :Array<object> = null;
   constructor() { 
     this.menuVisible = true;
@@ -27,8 +29,9 @@ export class DraggableComponent implements OnInit {
    
         this.toolbarItems = [
             {
-                location: 'after',
+                location: 'before',
                 widget: 'dxButton',
+                locateInMenu: 'never',
                 options: {
                     icon: 'menu',
                     onClick: () => {
@@ -37,7 +40,7 @@ export class DraggableComponent implements OnInit {
                 }
             }, {
                 location: 'center',
-                template: 'title'
+              
             }
         ];
 
@@ -66,295 +69,7 @@ export class DraggableComponent implements OnInit {
  
 
   ngOnInit() {
-    // this.Items= [{
-    //     id: "1_1",
-    //     text: "Super Mart of the West",
-    //     expanded: true,
-    //     items: [{
-    //         id: "1_1_1",
-    //         text: "Video Players",
-    //         items: [{
-    //             id: "1_1_1_1",
-    //             text: "HD Video Player",
-    //             price: 220,
-    //             image: "images/products/1.png"
-    //         }, {
-    //             id: "1_1_1_2",
-    //             text: "SuperHD Video Player",
-    //             image: "images/products/2.png",
-    //             price: 270
-    //         }]
-    //     }, {
-    //         id: "1_1_2",
-    //         text: "Televisions",
-    //         expanded: true,
-    //         items: [{
-    //             id: "1_1_2_1",
-    //             text: "SuperLCD 42",
-    //             image: "images/products/7.png",
-    //             price: 1200
-    //         }, {
-    //             id: "1_1_2_2",
-    //             text: "SuperLED 42",
-    //             image: "images/products/5.png",
-    //             price: 1450
-    //         }, {
-    //             id: "1_1_2_3",
-    //             text: "SuperLED 50",
-    //             image: "images/products/4.png",
-    //             price: 1600
-    //         }, {
-    //             id: "1_1_2_4",
-    //             text: "SuperLCD 55",
-    //             image: "images/products/6.png",
-    //             price: 1350
-    //         }, {
-    //             id: "1_1_2_5",
-    //             text: "SuperLCD 70",
-    //             image: "images/products/9.png",
-    //             price: 4000
-    //         }]
-    //     }, {
-    //         id: "1_1_3",
-    //         text: "Monitors",
-    //         expanded: true,
-    //         items: [{
-    //             id: "1_1_3_1",
-    //             text: "19\"",
-    //             expanded: true,
-    //             items: [{
-    //                 id: "1_1_3_1_1",
-    //                 text: "DesktopLCD 19",
-    //                 image: "images/products/10.png",
-    //                 price: 160
-    //             }]
-    //         }, {
-    //             id: "1_1_3_2",
-    //             text: "21\"",
-    //             items: [{
-    //                 id: "1_1_3_2_1",
-    //                 text: "DesktopLCD 21",
-    //                 image: "images/products/12.png",
-    //                 price: 170
-    //             }, {
-    //                 id: "1_1_3_2_2",
-    //                 text: "DesktopLED 21",
-    //                 image: "images/products/13.png",
-    //                 price: 175
-    //             }]
-    //         }]
-    //     }, {
-    //         id: "1_1_4",
-    //         text: "Projectors",
-    //         items: [{
-    //             id: "1_1_4_1",
-    //             text: "Projector Plus",
-    //             image: "images/products/14.png",
-    //             price: 550
-    //         }, {
-    //             id: "1_1_4_2",
-    //             text: "Projector PlusHD",
-    //             image: "images/products/15.png",
-    //             price: 750
-    //         }]
-    //     }]
-
-    // }, {
-    //     id: "1_2",
-    //     text: "Braeburn",
-    //     items: [{
-    //         id: "1_2_1",
-    //         text: "Video Players",
-    //         items: [{
-    //             id: "1_2_1_1",
-    //             text: "HD Video Player",
-    //             image: "images/products/1.png",
-    //             price: 240
-    //         }, {
-    //             id: "1_2_1_2",
-    //             text: "SuperHD Video Player",
-    //             image: "images/products/2.png",
-    //             price: 300
-    //         }]
-    //     }, {
-    //         id: "1_2_2",
-    //         text: "Televisions",
-    //         items: [{
-    //             id: "1_2_2_1",
-    //             text: "SuperPlasma 50",
-    //             image: "images/products/3.png",
-    //             price: 1800
-    //         }, {
-    //             id: "1_2_2_2",
-    //             text: "SuperPlasma 65",
-    //             image: "images/products/8.png",
-    //             price: 3500
-    //         }]
-    //     }, {
-    //         id: "1_2_3",
-    //         text: "Monitors",
-    //         items: [{
-    //             id: "1_2_3_1",
-    //             text: "19\"",
-    //             items: [{
-    //                 id: "1_2_3_1_1",
-    //                 text: "DesktopLCD 19",
-    //                 image: "images/products/10.png",
-    //                 price: 170
-    //             }]
-    //         }, {
-    //             id: "1_2_3_2",
-    //             text: "21\"",
-    //             items: [{
-    //                 id: "1_2_3_2_1",
-    //                 text: "DesktopLCD 21",
-    //                 image: "images/products/12.png",
-    //                 price: 180
-    //             }, {
-    //                 id: "1_2_3_2_2",
-    //                 text: "DesktopLED 21",
-    //                 image: "images/products/13.png",
-    //                 price: 190
-    //             }]
-    //         }]
-    //     }]
-
-    // }, {
-    //     id: "1_3",
-    //     text: "E-Mart",
-    //     items: [{
-    //         id: "1_3_1",
-    //         text: "Video Players",
-    //         items: [{
-    //             id: "1_3_1_1",
-    //             text: "HD Video Player",
-    //             image: "images/products/1.png",
-    //             price: 220
-    //         }, {
-    //             id: "1_3_1_2",
-    //             text: "SuperHD Video Player",
-    //             image: "images/products/2.png",
-    //             price: 275
-    //         }]
-    //     }, {
-    //         id: "1_3_3",
-    //         text: "Monitors",
-    //         items: [{
-    //             id: "1_3_3_1",
-    //             text: "19\"",
-    //             items: [{
-    //                 id: "1_3_3_1_1",
-    //                 text: "DesktopLCD 19",
-    //                 image: "images/products/10.png",
-    //                 price: 165
-    //             }]
-    //         }, {
-    //             id: "1_3_3_2",
-    //             text: "21\"",
-    //             items: [{
-    //                 id: "1_3_3_2_1",
-    //                 text: "DesktopLCD 21",
-    //                 image: "images/products/12.png",
-    //                 price: 175
-    //             }]
-    //         }]
-    //     }]
-    // }, {
-    //     id: "1_4",
-    //     text: "Walters",
-    //     items: [{
-    //         id: "1_4_1",
-    //         text: "Video Players",
-    //         items: [{
-    //             id: "1_4_1_1",
-    //             text: "HD Video Player",
-    //             image: "images/products/1.png",
-    //             price: 210
-    //         }, {
-    //             id: "1_4_1_2",
-    //             text: "SuperHD Video Player",
-    //             image: "images/products/2.png",
-    //             price: 250
-    //         }]
-    //     }, {
-    //         id: "1_4_2",
-    //         text: "Televisions",
-    //         items: [{
-    //             id: "1_4_2_1",
-    //             text: "SuperLCD 42",
-    //             image: "images/products/7.png",
-    //             price: 1100
-    //         }, {
-    //             id: "1_4_2_2",
-    //             text: "SuperLED 42",
-    //             image: "images/products/5.png",
-    //             price: 1400
-    //         }, {
-    //             id: "1_4_2_3",
-    //             text: "SuperLED 50",
-    //             image: "images/products/4.png",
-    //             price: 1500
-    //         }, {
-    //             id: "1_4_2_4",
-    //             text: "SuperLCD 55",
-    //             image: "images/products/6.png",
-    //             price: 1300
-    //         }, {
-    //             id: "1_4_2_5",
-    //             text: "SuperLCD 70",
-    //             image: "images/products/9.png",
-    //             price: 4000
-    //         }, {
-    //             id: "1_4_2_6",
-    //             text: "SuperPlasma 50",
-    //             image: "images/products/3.png",
-    //             price: 1700
-    //         }]
-    //     }, {
-    //         id: "1_4_3",
-    //         text: "Monitors",
-    //         items: [{
-    //             id: "1_4_3_1",
-    //             text: "19\"",
-    //             items: [{
-    //                 id: "1_4_3_1_1",
-    //                 text: "DesktopLCD 19",
-    //                 image: "images/products/10.png",
-    //                 price: 160
-    //             }]
-    //         }, {
-    //             id: "1_4_3_2",
-    //             text: "21\"",
-    //             items: [{
-    //                 id: "1_4_3_2_1",
-    //                 text: "DesktopLCD 21",
-    //                 image: "images/products/12.png",
-    //                 price: 170
-    //             }, {
-    //                 id: "1_4_3_2_2",
-    //                 text: "DesktopLED 21",
-    //                 image: "images/products/13.png",
-    //                 price: 180
-    //             }]
-    //         }]
-    //     }, {
-    //         id: "1_4_4",
-    //         text: "Projectors",
-    //         items: [{
-    //             id: "1_4_4_1",
-    //             text: "Projector Plus",
-    //             image: "images/products/14.png",
-    //             price: 550
-    //         }, {
-    //             id: "1_4_4_2",
-    //             text: "Projector PlusHD",
-    //             image: "images/products/15.png",
-    //             price: 750
-    //         }]
-    //     }]
-
-    // }];
-
+      
     this.Items= this.CreateTreeView(TreeViewService.GetTreeDataFromService());
 }
 
@@ -402,7 +117,9 @@ public closeNav() {
         return;
       }
       else{
+          
         window.alert(event.node.text);
+        this.menuVisible = !this.menuVisible;
       }
     }
 
