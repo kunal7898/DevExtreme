527bdfb7860542a93a8fdc88ec547033824d125a
Kunal Singh <kunal.singh@excellonsoft.com>
2018-04-03 18:55:23 +0530

    changes

--- src/app/custom-data-source-form/custom-data-source-form.component.ts
+++ src/app/custom-data-source-form/custom-data-source-form.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { CustomFormService } from './CustomFromService';
-import * as $ from 'jquery';
-import { Http } from '@angular/http';
-import CustomStore from 'devextreme/data/custom_store';
-import DataSource from 'devextreme/data/data_source';
+import { Component, OnInit } from "@angular/core";
+import { CustomFormService } from "./CustomFromService";
+import * as $ from "jquery";
+import { Http } from "@angular/http";
+import CustomStore from "devextreme/data/custom_store";
+import DataSource from "devextreme/data/data_source";
 
 @Component({
-  selector: 'app-custom-data-source-form',
-  templateUrl: './custom-data-source-form.component.html',
-  styleUrls: ['./custom-data-source-form.component.css']
+  selector: "app-custom-data-source-form",
+  templateUrl: "./custom-data-source-form.component.html",
+  styleUrls: ["./custom-data-source-form.component.css"]
 })
 export class CustomDataSourceFormComponent implements OnInit {
   FirstgroupCount: number = 0;
@@ -16,29 +16,26 @@ export class CustomDataSourceFormComponent implements OnInit {
   formData: any;
   items: any[];
   tabs: any[];
-  dataSource : DataSource;
-  gridControl:any;
-  constructor(private http: Http) { 
+  dataSource: DataSource;
+  gridControl: any;
+  BlankDatasource: DataSource;
+  constructor(private http: Http) {
     this.dataSource = this.GetCustomDataSource();
     this.dataSource.load();
-    
+
     this.formData = this.SetFormData();
     this.items = this.LoadInnerItems(this.LoadHeaderItems());
     this.tabs = CustomFormService.LoadTabs();
   }
-  first : boolean=true;
-  ngOnInit() {
-    
-   
-  }
+  first: boolean = true;
+  ngOnInit() {}
 
   private SetFormData() {
-    var Obj = [{ "State": "" }];
+    var Obj = [{ State: "" }];
     return Obj;
   }
 
   private LoadHeaderItems(): Array<object> {
-
     let viewresolver = new CustomFormService();
     let Items = Array<object>();
     viewresolver.LoadMetaData().forEach(element => {
@@ -48,7 +45,7 @@ export class CustomDataSourceFormComponent implements OnInit {
           colCount: element["colCount"],
           itemType: "group",
           items: []
-        })
+        });
         this.FirstgroupCount++;
       }
       if (element["cssClass"] == "second-group" && this.SecondGroupCount == 0) {
@@ -57,7 +54,7 @@ export class CustomDataSourceFormComponent implements OnInit {
           colCount: element["colCount"],
           itemType: "group",
           items: []
-        })
+        });
         this.SecondGroupCount++;
       }
     });
@@ -65,94 +62,117 @@ export class CustomDataSourceFormComponent implements OnInit {
     return Items;
   }
 
-
-
   private LoadInnerItems(Inneritems: any): Array<object> {
     let viewresolver = new CustomFormService();
 
     viewresolver.LoadMetaData().forEach(element => {
-
       if (this.SecondGroupCount == 1 && element["cssClass"] == "second-group") {
         Inneritems[0].items.push({
           dataField: element["code"],
           editorType: this.getEditorType(element["AttributeType"]),
-          editorOptions: this.getEditorOptions(this.getEditorType(element["AttributeType"]), element["PicklistId"], element["code"]),
-          validationRules: this.getMandatoryFieldsValidation(element["code"], element["IsMandatory"], element["length"], element["IsCustomValidation"], element["validationCallback"]),
-        })
+          editorOptions: this.getEditorOptions(
+            this.getEditorType(element["AttributeType"]),
+            element["PicklistId"],
+            element["code"]
+          ),
+          validationRules: this.getMandatoryFieldsValidation(
+            element["code"],
+            element["IsMandatory"],
+            element["length"],
+            element["IsCustomValidation"],
+            element["validationCallback"]
+          )
+        });
       }
     });
 
     return Inneritems;
   }
 
-  public  getColumns(){
-    
+  public getColumns() {
     let columns = new Array<object>();
     let obj = new CustomFormService();
-    
-    obj.getColumns().forEach((eachObj) => {
-        
-         if (eachObj["AttributeType"]=='number'){
-         columns.push({width :150, allowFiltering:true,allowSorting:true,dataField:eachObj["code"],caption:eachObj["code"]})
-     }
-         if (eachObj["AttributeType"]=='string'){
-         columns.push({width :150, allowFiltering:true,allowSorting:true,dataField:eachObj["code"],caption:eachObj["code"]})
-     }
-     if (eachObj["AttributeType"]=='date'){
-         columns.push({width :150, allowFiltering:true,allowSorting:true,dataField:eachObj["code"],caption:eachObj["code"],dataType:'date'})
-     }
- 
-     if(eachObj["AttributeType"]=='Lookup'){
-         let tempData = JSON.parse(localStorage.getItem(eachObj["PicklistMasterId"]));  
-         columns.push({width :150, allowFiltering:true,allowSorting:true, lookup: {
-             dataSource:tempData,
-             valueExpr: "Name",
-             displayExpr: "Name"
-         },dataField:eachObj["code"],caption:eachObj["code"]});
-         
-     }
-    })
- 
-    return columns;
- 
-    }
 
-  public getMandatoryFieldsValidation(Code, IsMandatory, length, IsCustomValidation, validationCallback): any {
+    obj.getColumns().forEach(eachObj => {
+      if (eachObj["AttributeType"] == "number") {
+        columns.push({
+          width: 150,
+          allowFiltering: true,
+          allowSorting: true,
+          dataField: eachObj["code"],
+          caption: eachObj["code"]
+        });
+      }
+      if (eachObj["AttributeType"] == "string") {
+        columns.push({
+          width: 150,
+          allowFiltering: true,
+          allowSorting: true,
+          dataField: eachObj["code"],
+          caption: eachObj["code"]
+        });
+      }
+      if (eachObj["AttributeType"] == "date") {
+        columns.push({
+          width: 150,
+          allowFiltering: true,
+          allowSorting: true,
+          dataField: eachObj["code"],
+          caption: eachObj["code"],
+          dataType: "date"
+        });
+      }
+
+      if (eachObj["AttributeType"] == "Lookup") {
+        let tempData = JSON.parse(
+          localStorage.getItem(eachObj["PicklistMasterId"])
+        );
+        columns.push({
+          width: 150,
+          allowFiltering: true,
+          allowSorting: true,
+          lookup: {
+            dataSource: tempData,
+            valueExpr: "Name",
+            displayExpr: "Name"
+          },
+          dataField: eachObj["code"],
+          caption: eachObj["code"]
+        });
+      }
+    });
+
+    return columns;
+  }
+
+  public getMandatoryFieldsValidation(
+    Code,
+    IsMandatory,
+    length,
+    IsCustomValidation,
+    validationCallback
+  ): any {
     let validationRule = Array<object>();
     if (IsMandatory == true) {
       validationRule.push({
         type: "required",
         message: Code + " is required"
-      })
+      });
     }
 
-
-    return validationRule
-
+    return validationRule;
   }
 
-
   public getEditorType(Attributetype): any {
-    if (Attributetype == "lookup")
-      return "dxSelectBox";
-    if (Attributetype == "Date")
-      return "dxDateBox";
-    if (Attributetype == "textarea")
-      return "dxTextArea";
-    if (Attributetype == "checkbox")
-      return "dxCheckBox";
-    if (Attributetype == "radiobox")
-      return "dxRadioGroup";
-    if (Attributetype == "DataGrid")
-      return "dxDataGrid";
-      if (Attributetype == "SaveButton")
-      return "dxButton";
-    if (Attributetype == "Tab")
-      return "dxTabs";
-
-    else
-      return null;
-
+    if (Attributetype == "lookup") return "dxSelectBox";
+    if (Attributetype == "Date") return "dxDateBox";
+    if (Attributetype == "textarea") return "dxTextArea";
+    if (Attributetype == "checkbox") return "dxCheckBox";
+    if (Attributetype == "radiobox") return "dxRadioGroup";
+    if (Attributetype == "DataGrid") return "dxDataGrid";
+    if (Attributetype == "SaveButton") return "dxButton";
+    if (Attributetype == "Tab") return "dxTabs";
+    else return null;
   }
 
   public getEditorOptions(Type, PicklistId, Code): any {
@@ -162,92 +182,86 @@ export class CustomDataSourceFormComponent implements OnInit {
         displayExpr: "OrderNumber",
         // valueExpr: "ID",
         searchEnabled: true,
-        onSelectionChanged: function (e) {
+        onSelectionChanged: function(e) {
           window.alert("event fired");
           this.popupVisible = false;
         },
-        onValueChanged: function (e) {
+        onValueChanged: function(e) {
           var get = this.DataSource;
           window.alert("event fired");
           this.popupVisible = false;
         }
       };
-      if(Type=="dxButton"){
-        var componentRef = this;
-        return {
-          text: 'Save Data',
-        onClick:function(e){
+    if (Type == "dxButton") {
+      var componentRef = this;
+      return {
+        text: "Save Data",
+        onClick: function(e) {
           componentRef.SaveFormData(e);
         }
-        };
-      }
-    if (Type == "dxDataGrid"){
+      };
+    }
+    if (Type == "dxDataGrid") {
       var componentvalue = this;
       return {
-        dataSource: this.dataSource ,
-        columns:this.getColumns(),
-        cacheEnabled:true,
+        dataSource: this.dataSource,
+        columns: this.getColumns(),
+        cacheEnabled: true,
         showRowLines: true,
         showBorders: true,
         height: "500",
-        width:"100%",
+        width: "100%",
         sorting: {
           mode: "multiple"
         },
+        twoWayBindingEnabled: true,
         editing: {
           mode: "batch",
           allowUpdating: true,
           allowDeleting: true,
           allowAdding: true
-      },
-      onContentReady: function (e) {
-         componentvalue.gridControl =  e.component;
-        if(componentvalue.first){
-          componentvalue.first= false;
-           e.component.addRow();  
-          // componentvalue.dataSource.items().unshift({
-          //   "OrderNumber":"",
-          //   "SaleAmount":"",
-          //   "StoreCity":"",
-          //   "StoreState":"",
-          //   "OrderDate":"",
-          //   "Employee":""
-          // }) ;
-         
-        }
-       },
-       onEditorPreparing:function(options,items){
-         
-        //  var Currentoptions= options;
-        //      options.editorOptions.onValueChanged = function(e,rowIndex){
-        //        Currentoptions.setValue(e.value);
-        //        var DataSource =  componentvalue.dataSource.items();
-        //        DataSource[ e.element.parentElement.parentElement.parentElement.rowIndex]=Currentoptions.row.data;
-        //        DataSource[ e.element.parentElement.parentElement.parentElement.rowIndex].IsDirty= true;
-               
-        //     }
-       },
-       onToolbarPreparing: function (e) {
-        var toolbarItems = e.toolbarOptions.items;
-        // Modifies an existing item
-        toolbarItems.forEach((element, index) => {
-          if(element.name=="addRowButton"){
-            element.options.type="success";
-            element.options.icon=null;
-            element.showText="always";
-          } else{
-            element.visible=false;
+        },
+        onContentReady: function(e) {
+          componentvalue.gridControl = e.component;
+          if (componentvalue.first) {
+            componentvalue.first = false;
+            // e.component.addRow();
           }
-      });
-      
-    },
-        cellTemplate: function (container, options) {
-
-          $('<a/>').addClass('dx-link')
+        },
+        onEditorPreparing: function(options, items) {
+          var CurrentOptions = options;
+          options.editorOptions.onValueChanged = function(e, rowIndex) {
+            CurrentOptions.setValue(e.value);
+            if (
+              e.element.parentElement.parentElement.parentElement.rowIndex >
+                CurrentOptions.component._controllers.data._items.length - 2 &&
+              CurrentOptions.index == CurrentOptions.component.columnCount() - 1
+            ) {
+              CurrentOptions.component.addRow(CurrentOptions.component._controllers.data._items[CurrentOptions.component._controllers.data._items.length-1].data.OrderNumber);
+             
+              //  var CurrentAddedRow = CurrentOptions.component._controllers.data._items.shift();
+              //  CurrentOptions.component._controllers.data._items.push(CurrentAddedRow);
+            }
+          };
+        },
+        onToolbarPreparing: function(e) {
+          var toolbarItems = e.toolbarOptions.items;
+          // Modifies an existing item
+          toolbarItems.forEach((element, index) => {
+            if (element.name == "addRowButton") {
+              element.options.type = "success";
+              element.options.icon = null;
+              element.showText = "always";
+            } else {
+              element.visible = false;
+            }
+          });
+        },
+        cellTemplate: function(container, options) {
+          $("<a/>")
+            .addClass("dx-link")
             .text(options.text)
-            .click('dxclick', function () {
-
-            })
+            .click("dxclick", function() {})
             .appendTo(container);
         },
         allowColumnReordering: true,
@@ -258,90 +272,78 @@ export class CustomDataSourceFormComponent implements OnInit {
         remoteOperations: {
           paging: true
         }
-      }
-    }
-    
-   
-    else
-      return null;
+      };
+    } else return null;
   }
 
-
-SaveFormData(e){
-  var rows = this.gridControl.getController('editing')._editData; 
-  if (this.gridControl.hasEditData())
-  var rows = this.gridControl.getController('editing')._editData;
-  for (var i = 0; i < rows.length; i++) {
-  console.log(rows);
-}
-
-
-
-// if (editData[i].type === 'update') {
-// rowCount++;
-// }
-}
-
-// this.gridControl.saveEditData().done(function (data) {
-// console.log(data);
-// }); 
+  SaveFormData(e) {
+    var rows = this.gridControl.getController("editing")._editData;
+    if (this.gridControl.hasEditData())
+      var rows = this.gridControl.getController("editing")._editData;
+    for (var i = 0; i < rows.length; i++) {
+      console.log(rows);
+    }
+  }
 
   GetCustomDataSource() {
     var http = this.http;
     var ComponentRef = this;
     return new DataSource({
-      
-      store: new CustomStore(
-        {
-          
-        key:"OrderNumber",
-      
-        load: function (loadOptions: any) {
+      store: new CustomStore({
+        key: "OrderNumber",
 
-          var params = '?';
+        load: function(loadOptions: any) {
+          var params = "?";
 
-          params += 'skip=' + loadOptions.skip || 0;
-          params += '&take=' + loadOptions.take || 10;
+          params += "skip=" + loadOptions.skip || 0;
+          params += "&take=" + loadOptions.take || 10;
 
-          return http.get('https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems' + params)
+          return http
+            .get(
+              "https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems" +
+                params
+            )
             .toPromise()
             .then(response => {
               var json = response.json();
+              json.items.push({ OrderNumber: 123 });
               console.log(json.items);
               return {
                 totalCount: json.totalCount,
                 data: json.items
               };
             });
-          
-        }, 
-        update: function(key,value) {
+        },
+        update: function(key, value) {
           // return http.put("https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems" + encodeURIComponent(key), value)
           // .toPromise();
           return null;
-         
-        },insert:function(values){
+        },
+        insert: function(values) {
           // return http.post("https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems", values)
           // .toPromise();
           return null;
-        },
+        }
       }),
       paginate: true,
       pageSize: 10
     });
   }
 
-
   GetCustomDataSourceSample() {
     var http = this.http;
     return new CustomStore({
-      load: function (loadOptions: any) {
-        var params = '?';
+      load: function(loadOptions: any) {
+        var params = "?";
 
-        params += 'skip=' + loadOptions.skip || 0;
-        params += '&take=' + loadOptions.take || 10;
+        params += "skip=" + loadOptions.skip || 0;
+        params += "&take=" + loadOptions.take || 10;
 
-        return http.get('https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems' + params)
+        return http
+          .get(
+            "https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems" +
+              params
+          )
           .toPromise()
           .then(response => {
             var json = response.json();
@@ -351,26 +353,39 @@ SaveFormData(e){
               data: json.items
             };
           });
-      },
+      }
     });
   }
-
 
   public LoadMetaData(): Array<object> {
     let Values = new Array<object>();
     Values.push(
-
-      { code: 'State', Name: 'State', AttributeType: 'lookup', PicklistId: 1, IsMandatory: true, cssClass: "second-group", colCount: null, length: null, IsCustomValidation: false, validationCallback: null },
+      {
+        code: "State",
+        Name: "State",
+        AttributeType: "lookup",
+        PicklistId: 1,
+        IsMandatory: true,
+        cssClass: "second-group",
+        colCount: null,
+        length: null,
+        IsCustomValidation: false,
+        validationCallback: null
+      },
       // {code:'Grid',Name:'Grid',AttributeType:'DataGrid',PicklistId:1,IsMandatory:true, cssClass: "second-group",colCount: null,length:null,IsCustomValidation:false,validationCallback:null},
-      { code: 'Tabs', Name: 'Tab', AttributeType: 'Tab', PicklistId: 1, IsMandatory: true, cssClass: "second-group", colCount: null, length: null, IsCustomValidation: false, validationCallback: null }
-
-    )
+      {
+        code: "Tabs",
+        Name: "Tab",
+        AttributeType: "Tab",
+        PicklistId: 1,
+        IsMandatory: true,
+        cssClass: "second-group",
+        colCount: null,
+        length: null,
+        IsCustomValidation: false,
+        validationCallback: null
+      }
+    );
     return Values;
   }
-
- 
-
-
-
-
 }
